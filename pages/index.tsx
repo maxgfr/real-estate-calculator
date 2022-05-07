455250d5758ecd0e69eba34cea5897088c1eb292
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-05-07 09:28:55 +0200

    fix: url

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   FormControl,
   FormLabel,
-  Input,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
@@ -12,15 +11,30 @@ import {
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 type State = {
   housingPrice: number;
   notaryFees: number;
+  houseWorks: number;
 };
 
 const Home: NextPage = () => {
-  const [value, setValue] = React.useState<State | {}>({});
+  const router = useRouter();
+  const [state, setState] = React.useState<State>({} as State);
+
+  useEffect(() => {
+    setState(router.query as unknown as State);
+  }, [router.query]);
+
+  const onChangeState = (key: string, value: number) => {
+    const parseValue = isNaN(value) ? 0 : value;
+    setState({ ...state, [key]: parseValue });
+    router.replace({
+      query: { ...router.query, [key]: parseValue },
+    });
+  };
 
   return (
     <Box
@@ -33,41 +47,61 @@ const Home: NextPage = () => {
       <Head>
         <title>Renta Immo</title>
       </Head>
-      <Text alignSelf={"center"} fontSize="6xl">
+      <Text alignSelf={"center"} fontSize="6xl" marginBottom={"4"}>
         Renta-immo
       </Text>
-      <Box display="flex" flexDirection="column">
-        <FormControl variant="floating" isRequired>
-          <FormLabel>Prix du logement</FormLabel>
-          <NumberInput
-            step={1000}
-            onChange={(_valueString, valueNumber) =>
-              setValue({ ...value, housingPrice: valueNumber })
-            }
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </FormControl>
-        <FormControl variant="floating" isRequired marginTop={"20px"}>
-          <FormLabel>Frais de notaire</FormLabel>
-          <NumberInput
-            step={500}
-            onChange={(_valueString, valueNumber) =>
-              setValue({ ...value, notaryFees: valueNumber })
-            }
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </FormControl>
-      </Box>
+      <Text fontSize="3xl" fontWeight={"bold"} marginBottom={"2"}>
+        Achat
+      </Text>
+      <FormControl variant="floating" isRequired>
+        <FormLabel>Prix du logement</FormLabel>
+        <NumberInput
+          step={1000}
+          min={0}
+          onChange={(_valueString, valueNumber) =>
+            onChangeState("housingPrice", valueNumber)
+          }
+          value={state.housingPrice}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+      <FormControl variant="floating" isRequired marginTop={"20px"}>
+        <FormLabel>Frais de notaire</FormLabel>
+        <NumberInput
+          step={500}
+          onChange={(_valueString, valueNumber) =>
+            onChangeState("notaryFees", valueNumber)
+          }
+          value={state.notaryFees}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+      <FormControl variant="floating" isRequired marginTop={"20px"}>
+        <FormLabel>Travaux</FormLabel>
+        <NumberInput
+          step={500}
+          onChange={(_valueString, valueNumber) =>
+            onChangeState("houseWorks", valueNumber)
+          }
+          value={state.houseWorks}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
     </Box>
   );
 };
--- utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const objectToUrlParams = (obj: Record<string, any>) => {
-  const params = Object.keys(obj)
-    .map(key => {
-      return `${key}=${obj[key]}`;
-    })
-    .join("&");
-  return params;
-};
